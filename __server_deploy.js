7fa7da9af83cf1c359e89abc06037c6565df683e
unknown <nazruldigitaladd@gmail.com>
2022-04-29 19:13:13 +0600

    first commit

---
+++ __server_deploy.js
@@ -0,0 +1,23 @@
+/**
+ * --------------------------------
+ * ONE TIME for your computer
+ * --------------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------------------
+ * For each project one time
+ * --------------------------------
+ * 1. heroku create
+ * 2. make sure you : git add . git commit. git push
+ * 3. git push heroku
+ * 4. Go to Heroku dashboard > Current Project > settings ? reveal config
+ * 5. copy paste config vars from your .env file
+ * --------------------------------
+ * UPDATE SERVER new changes
+ * --------------------------------
+ * 1. Make change
+ * 2. make sure you : git add . git commit. git push
+ * 3.
+ */
